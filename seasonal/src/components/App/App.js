42e8004ac03e1d02c40176827541645aa7f86caf
marwaismail99 <ismailmarwa.563@gmail.com>
2022-02-17 15:51:29 +0000

    React-router installed and working

--- seasonal/src/components/App/App.js
+++ seasonal/src/components/App/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Homepage from "../Homepage/Homepage.js";
+import IngredientPage from "../IngredientPage/IngredientPage.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <h1>Hi.</h1>
+      <nav>
+        <Link to="/ingredients">Ingredients Page</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="ingredients" element={<IngredientPage />} />
+      </Routes>
     </div>
   );
 }
