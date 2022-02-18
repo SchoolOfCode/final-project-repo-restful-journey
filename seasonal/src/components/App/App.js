0a0ddc9e210e329dc55dcebd62c629602df2a0aa
karen derbyshire <karenderbyshire5@gmail.com>
2022-02-18 14:03:17 +0000

    hamburger menu

--- seasonal/src/components/App/App.js
+++ seasonal/src/components/App/App.js
@@ -5,9 +5,11 @@ import IngredientPage from "../IngredientPage/IngredientPage.js";
 import RecipePage from "../RecipePage/RecipePage.js";
 import SearchPage from "../SearchPage/SearchPage.js";
 import ShoppingList from "../ShoppingList/ShoppingList.js";
+import NavMenu from "../NavMenu/navmenu";
 
 function App() {
   return (
+
     <div className="App">
       <nav>
         <Link to="/">Homepage</Link>
@@ -15,6 +17,7 @@ function App() {
         <Link to="/recipes">Recipes</Link>
         <Link to="/search">Search</Link>
         <Link to="/shoppinglist">Shopping List</Link>
+        <Link to="/hamburger">hamburgermenu</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Homepage />} />
@@ -22,6 +25,7 @@ function App() {
         <Route path="recipes" element={<RecipePage />} />
         <Route path="search" element={<SearchPage />} />
         <Route path="shoppinglist" element={<ShoppingList />} />
+        <Route path="hamburger" element={<NavMenu />} />
       </Routes>
     </div>
   );
