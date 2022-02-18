b4ab2f6486a18a7bd242773aaca409c60821ca88
karen derbyshire <karenderbyshire5@gmail.com>
2022-02-18 15:37:49 +0000

    hamburger menu

--- seasonal/src/components/NavMenu/navmenu.js
+++ seasonal/src/components/NavMenu/navmenu.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../../global';
 import { theme } from '../../theme';
 import Burger  from "../Burger/burger.js"
 import Menu from '../Menu/menu.js';
+import { useOnClickOutside } from '../../hook';
 
 
 function NavMenu() {
 
     const [open, setOpen] = useState(false);
+    const node = useRef(); 
+    useOnClickOutside(node, () => setOpen(false));
+
+
   return (
     <ThemeProvider theme={theme}>
       <>
@@ -17,11 +22,11 @@ function NavMenu() {
         <div>
           <h1>Hello. This is burger menu tutorial</h1>
           <img src="https://image.flaticon.com/icons/svg/2016/2016012.svg" alt="burger icon" />
-          <small>Icon made by Freepik from www.flaticon.com</small>
+          
         </div>
-        <div>
+        <div ref={node}>
         <Burger open={open} setOpen={setOpen} />
-        <Menu open={open} setOpen={setOpen}/>
+        <Menu open={open} setOpen={setOpen} />
         </div>
       </>
     </ThemeProvider>
