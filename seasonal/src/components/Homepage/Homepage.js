6de892e278d69324b1afc000354fee79ff6427b0
marwaismail99 <ismailmarwa.563@gmail.com>
2022-02-18 09:50:19 +0000

    Setting up environmental variables

--- seasonal/src/components/Homepage/Homepage.js
+++ seasonal/src/components/Homepage/Homepage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useFetch } from "../hooks/useFetch.js";
 
 //test api used for testing useFetch custom hook. Will be replaced with ingredients back end.
-const api = "https://uselessfacts.jsph.pl/random.json?language=en";
+const api = process.env.API_CALL;
 
 function Homepage() {
   const [data] = useFetch(api);
