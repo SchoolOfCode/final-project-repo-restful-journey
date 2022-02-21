efe4ebb9a65a96f9aeeaaec695b3252b281e2bf8
marwaismail99 <ismailmarwa.563@gmail.com>
2022-02-21 16:44:06 +0000

    Nav menu styling + routes
    Co-authored-by: Luis <luiscvrodrigues@gmail.com>

--- seasonal/src/components/NavMenu/navmenu.js
+++ seasonal/src/components/NavMenu/navmenu.js
@@ -1,32 +1,23 @@
-import React, { useState, useRef } from 'react';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from '../../global';
-import { theme } from '../../theme';
-import Burger  from "../Burger/burger.js"
-import Menu from '../Menu/menu.js';
-import { useOnClickOutside } from '../../hook';
-
+import React, { useState, useRef } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../../global";
+import { menuTheme } from "../../theme";
+import Burger from "../Burger/burger.js";
+import Menu from "../Menu/menu.js";
+import { useOnClickOutside } from "../../hook";
 
 function NavMenu() {
-
-    const [open, setOpen] = useState(false);
-    const node = useRef(); 
-    useOnClickOutside(node, () => setOpen(false));
-
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  useOnClickOutside(node, () => setOpen(false));
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={menuTheme}>
       <>
         <GlobalStyles />
-        
-        <div>
-          <h1>Hello. This is burger menu tutorial</h1>
-          <img src="https://image.flaticon.com/icons/svg/2016/2016012.svg" alt="burger icon" />
-          
-        </div>
         <div ref={node}>
-        <Burger open={open} setOpen={setOpen} />
-        <Menu open={open} setOpen={setOpen} />
+          <Burger open={open} setOpen={setOpen} />
+          <Menu open={open} setOpen={setOpen} />
         </div>
       </>
     </ThemeProvider>
