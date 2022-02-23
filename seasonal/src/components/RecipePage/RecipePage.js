06dcb73e5a816c0a846555cf241ff1bf704de336
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-02-23 19:39:30 +0000

    fixes

--- seasonal/src/components/RecipePage/RecipePage.js
+++ seasonal/src/components/RecipePage/RecipePage.js
@@ -1,40 +1,54 @@
-import React from "react";
+import React from 'react';
 
-function RecipePage({userRecipe}) {
-  console.log (userRecipe)
-  return(
-    <div>
-    {/* <div>
-<img/>
-<h1>{userRecipe[0].recipe.label}</h1>
-<div/>
-
-<div>
-  <h3> Time To Cook</h3>
-</div>
-
-<div>
-  <h3>Amount of servings</h3>
-</div>
-
-<div>
-  <h3>
-    Nutritional Info
-  </h3>
-</div>
-
-  <h2>Ingredient: {ingredient}</h2>
-      <div>
-        <ul>
-          <li>Radish</li>
-        <li>Rocket</li>
-        <li>Beetroot</li>
-        <li>Lettuce</li>
-        </ul>
-       </div> */}
-       </div>
-  )
+function RecipePage({ userRecipe }) {
   
+  if (userRecipe.length) {
+    return (
+      <>
+        <div>
+          <img src={userRecipe[0].recipe.images.REGULAR.url} alt="userRecipe[0].recipe.label" />
+        </div>
+        <div>
+          <p>{userRecipe[0].recipe.label}</p>
+        </div>
+        <div>
+          <div>
+            <p><a href={userRecipe[0].recipe.url}>link to external recipe</a></p>
+          </div>
+          <div>
+            <p>{userRecipe[0].recipe.totalTime} minutes</p>
+          </div>
+          <div>
+            <p>{Math.round(userRecipe[0].recipe.totalNutrients.ENERC_KCAL.quantity)} kcal</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>Ingredients</p>
+          </div>
+          <div>
+            <ul>
+              {userRecipe[0].recipe.ingredientLines.map((item, i)=>{
+              return(
+                <div key={i} style={{display: 'flex', justifyContent: 'space-between'}}>
+                  <li >{item}</li>
+                  <button>+</button>
+                </div>
+                )
+              })}
+            </ul>
+          </div>
+        </div>          
+      </>
+    );
+  } else {
+    return (
+      <div>
+        {' '}
+        <p>Go Back and select a recipe</p>
+      </div>
+    );
+  }
 }
 
 export default RecipePage;
