d10b337129de333b7296d34be75f8fd8d1939f24
fonox1 <abdih0698@gmail.com>
2022-03-02 16:49:08 +0000

    png-update

--- seasonal/src/components/LogoutButton/Logout.js
+++ seasonal/src/components/LogoutButton/Logout.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
+    <Button 
+     size='md'
+    height='48px'
+    width='200px'
+    border='2px'
+    borderColor='#black' 
+    backgroundColor= '#ec9488' 
+    _hover={{ bg: '#red' }}
+
+   onClick={() => logout({ returnTo: window.location.origin })}>
       Log Out
-    </button>
+    </Button>
   );
 };
 
