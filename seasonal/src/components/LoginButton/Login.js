1b8a36283502d27f90badeaf190a612d1eb0387d
marwaismail99 <ismailmarwa.563@gmail.com>
2022-03-03 12:32:32 +0000

    Login page styling done

--- seasonal/src/components/LoginButton/Login.js
+++ seasonal/src/components/LoginButton/Login.js
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Profile from "../Profile/Profile";
 import LogoutButton from "../LogoutButton/Logout";
 import css from "./LoginButton.module.css";
-import { Button, ButtonGroup, Stack } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
@@ -11,23 +11,27 @@ const LoginButton = () => {
   return (
     <div className={css.loginPage}>
       <Profile />
+      <img
+        className={css.img}
+        src={require("../../Images/login-image.png")}
+        alt="fruits & veg"
+      />
+      <h1 className={css.welcome}>Welcome to</h1>
+      <h1 className={css.nourish}>nourish.</h1>
       <div className={css.logButtons}>
-        <Stack variant="outline" direction="column" spacing="10">
-          {/* <img src= '../../loginbanner.png'alt='loginbanner'/> */}
-          <Button
-            size="md"
-            height="48px"
-            width="200px"
-            border="2px"
-            borderColor="#black"
-            backgroundColor="#acf39d"
-            className={css.login}
-            onClick={() => loginWithRedirect()}
-          >
-            Log In
-          </Button>
-          <LogoutButton className={css.logout} />
-        </Stack>
+        <Button
+          size="md"
+          height="48px"
+          width="250px"
+          border="2px"
+          borderColor="#black"
+          backgroundColor="#acf39d"
+          className={css.login}
+          onClick={() => loginWithRedirect()}
+        >
+          Log In
+        </Button>
+        <LogoutButton className={css.logout} />
       </div>
     </div>
   );
--- seasonal/src/components/LogoutButton/Logout.js
+++ seasonal/src/components/LogoutButton/Logout.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import { Button, ButtonGroup } from '@chakra-ui/react'
-
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <Button 
-     size='md'
-    height='48px'
-    width='200px'
-    border='2px'
-    borderColor='#black' 
-    backgroundColor= '#ec9488' 
-    _hover={{ bg: '#red' }}
-
-   onClick={() => logout({ returnTo: window.location.origin })}>
+    <Button
+      size="md"
+      height="48px"
+      width="250px"
+      border="2px"
+      borderColor="#black"
+      backgroundColor="#ec9488"
+      _hover={{ bg: "#red" }}
+      onClick={() => logout({ returnTo: window.location.origin })}
+    >
       Log Out
     </Button>
   );
