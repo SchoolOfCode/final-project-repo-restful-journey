430534851593d0bf04d8fbd999897c185db92e59
ajp64 <antonyjparnell@gmail.com>
2022-03-04 15:19:50 +0000

    changed file path

--- seasonal/src/components/App/App.js
+++ seasonal/src/components/App/App.js
@@ -7,7 +7,7 @@ import SearchPage from "../SearchPage/SearchPage.js";
 import ShoppingList from "../ShoppingList/ShoppingList.js";
 import NavMenu from "../NavMenu/navmenu";
 import LoginButton from "../LoginButton/Login";
-import { About } from "../About/About.js";
+import { About } from "../About/about.js";
 import { Box } from "@chakra-ui/react";
 import { Logo } from "../logo/logo.js";
 import { useAuth0 } from "@auth0/auth0-react";
